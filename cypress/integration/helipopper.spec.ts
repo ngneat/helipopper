743f1f8801bd80b04300790b5532ea55e58e88ba
arturovt <arthurandrosovich@gmail.com>
2020-11-16 07:56:28 +0200

    test: fix Cypress tests related to scrolling

--- cypress/integration/helipopper.spec.ts
+++ cypress/integration/helipopper.spec.ts
@@ -1,3 +1,15 @@
+Cypress.on('scrolled', element => {
+  // When we do `cy.get()` the Cypress finds the element
+  // and scrolls down, thus this element becomes at the very top
+  // of the page.
+  // `tippy.js` will not show tooltips if they appeared in an invisible
+  // part of the window.
+  element.get(0).scrollIntoView({
+    block: 'center',
+    inline: 'center'
+  });
+});
+
 describe('@ngneat/helipopper', () => {
   beforeEach(() => {
     cy.visit('/').wait(200);
