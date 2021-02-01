54d0998a65e346aa30e90b104b57562f3d69b275
va-stefanek <mateusz.stefanczyk@valueadd.pl>
2021-02-01 08:46:15 +0100

    feat: add ngneat/overview to package.json by schematics

--- schematics/src/ng-add/index.ts
+++ schematics/src/ng-add/index.ts
@@ -1,11 +1,13 @@
-import { chain, Rule, SchematicContext, SchematicsException, Tree } from '@angular-devkit/schematics';
-import { createSourceFile, ScriptTarget, SourceFile } from 'typescript';
-import { LIB_NAME } from '../schematics.consts';
-import { addImportToModule, insertImport } from '../utils/ast-utils';
-import { InsertChange } from '../utils/change';
-import { findRootModule } from '../utils/find-module';
-import { getProject } from '../utils/projects';
-import { SchemaOptions } from './schema';
+import { chain, Rule, SchematicContext, SchematicsException, Tree } from "@angular-devkit/schematics";
+import { createSourceFile, ScriptTarget, SourceFile } from "typescript";
+import { LIB_NAME } from "../schematics.consts";
+import { addImportToModule, insertImport } from "../utils/ast-utils";
+import { InsertChange } from "../utils/change";
+import { findRootModule } from "../utils/find-module";
+import { getProject } from "../utils/projects";
+import { SchemaOptions } from "./schema";
+import { NodePackageInstallTask } from "@angular-devkit/schematics/tasks";
+import { addPackageToPackageJson } from "../utils/package";
 
 export function getModuleFile(host: Tree, options: SchemaOptions): SourceFile {
   const modulePath = options.module;
@@ -18,7 +20,7 @@ export function getModuleFile(host: Tree, options: SchemaOptions): SourceFile {
   if (text === null) {
     throw new SchematicsException(`File ${modulePath} does not exist.`);
   }
-  const sourceText = text.toString('utf-8');
+  const sourceText = text.toString("utf-8");
 
   return createSourceFile(modulePath, sourceText, ScriptTarget.Latest, true);
 }
@@ -39,7 +41,7 @@ export function applyChanges(host: Tree, path: string, changes: InsertChange[]):
 export function addImportsToModuleFile(options: SchemaOptions, imports: string[], file = LIB_NAME): Rule {
   return host => {
     const module = getModuleFile(host, options);
-    const importChanges = insertImport(module, options.module, imports.join(', '), file);
+    const importChanges = insertImport(module, options.module, imports.join(", "), file);
 
     return applyChanges(host, options.module, [importChanges] as InsertChange[]);
   };
@@ -54,15 +56,24 @@ export function addImportsToModuleDeclaration(options: SchemaOptions, imports: s
   };
 }
 
+function addModuleToPackageJson() {
+  return (host: Tree, context: SchematicContext) => {
+    addPackageToPackageJson(host, "dependencies", "@ngneat/overview", "^1.0.0");
+    context.addTask(new NodePackageInstallTask());
+    return host;
+  };
+}
+
 export default function(options: SchemaOptions): Rule {
   return (host: Tree, context: SchematicContext) => {
     const project = getProject(host, options.project);
-    const sourceRoot = (project && project.sourceRoot) || 'src';
+    const sourceRoot = (project && project.sourceRoot) || "src";
     options.module = findRootModule(host, options.module, sourceRoot) as string;
 
     return chain([
-      addImportsToModuleFile(options, ['TippyModule']),
-      addImportsToModuleDeclaration(options, ['TippyModule.forRoot()'])
+      addModuleToPackageJson,
+      addImportsToModuleFile(options, ["TippyModule"]),
+      addImportsToModuleDeclaration(options, ["TippyModule.forRoot()"])
     ])(host, context);
   };
 }
